b9e55c8820291501cc0d3c92e6e0fcdbf10bcdca
Aram-Vn <aramvardanyan13@gmail.com>
2023-12-25 08:42:55 +0400

    inordered

--- Vec_declaration.h
+++ Vec_declaration.h
@@ -9,7 +9,8 @@ template <class T>
 class Vec
 {
 public:
-	class const_iterator
+	// nested iterator class for const Vec objects
+	class const_iterator 
 	{
 	public:
 		const_iterator(); //
@@ -46,11 +47,7 @@ public:
 	};
 
 public:
-	using const_itr = const_iterator;
-	const const_itr c_begin() const;
-	const const_itr c_end() const;
-
-public:
+	//nested random access iterator for Vec class
 	class random_access_itr
 	{
 	public:
@@ -92,11 +89,21 @@ public:
 	};
 
 public:
+	// Iterators
+
+	// for const Vec obj 
+	using const_itr = const_iterator; //
+	const const_itr c_begin() const; //
+	const const_itr c_end() const; //
+
+	// for nonconst Vec obj
 	using itr = random_access_itr;
 	itr begin(); //
 	itr end(); //
 
 public:
+	// Member functions
+
 	Vec(); // defolt constructor
 	~Vec(); // destructor
 	Vec(std::initializer_list<T> init_list); // initializer_list constructor
@@ -104,43 +111,49 @@ public:
 	Vec(const Vec& other); // copy constructor 
 	Vec(Vec&& other) noexcept; //move constructor
 
-	T& operator[](long long ind) const; //subscript operator overload
 	Vec& operator=(const Vec& other); // copy assignment operator overload
 	Vec& operator=(Vec&& other) noexcept; // move assignment operator overload
 
+	void assign(int amount, T val = 0); /*amount (the number of elements to assign) and val (the value to assign to each element).
+										It updates the size of the vector (m_size) to the specified amount */
+
+
 public:
-	size_t Size() const; //returns m_size
-	size_t Max_size() const; //Returns the maximum number of elements that the vector can hold
-	size_t Capacity() const; //return m_cap
-	bool Empty() ; // return true if empty	
-	void Reserve(size_t new_size);// make m_cap ecual to new_size if new_size > m_cap
-	void shrink_to_fit();//Requests the container to reduce its capacity to fit its size.
-	
+	// Element access
 	T& at(int ind) const; // Returns a reference to the element at position ind in the vector.
+	T& operator[](long long ind) const; //subscript operator overload
 	T& front() const; // Returns a reference to the first element in the Vec
 	T& back() const; // Returns a reference to the last element in the vector
 	T* data()  noexcept; //Returns a direct pointer to the memory array used internally by the vector to store its owned elements.
 	const T* data() const noexcept; /*(for const objects) Returns a direct pointer to the memory array used 
-					internally by the vector to store its owned elements.*/
-	void assign(int amount, T val = 0); /*amount (the number of elements to assign) and val (the value to assign to each element).
-					     It updates the size of the vector (m_size) to the specified amount */
-	void push_back(const T& val); // Adds a new element(num) at the end of the vector,
-	void pop_back(); // Removes the last element in the vector, redusing m_size by 1,
-	void insert(int ind, const T& val); // inserts the element(val) at given ind, increasing size by 1
-	void erase(int ind); // erase element at given position(ind), decreasing size by 1 	
-	void swap (Vec& other); /*Exchanges the content of the container by the content of other, which is another Vec object of the same 
-				type. Sizes may differ.*/
-	void clear(); //Removes all elements from the vector, leaving the container with a size of 0.
+									internally by the vector to store its owned elements.*/
 
+	// Capacity
+	bool Empty() ; // return true if empty	
+	size_t Size() const; //returns m_size
+	size_t Max_size() const; //Returns the maximum number of elements that the vector can hold
+	void Reserve(size_t new_size);// make m_cap ecual to new_size if new_size > m_cap
+	size_t Capacity() const; //return m_cap
+	void shrink_to_fit();//Requests the container to reduce its capacity to fit its size.
+	
+	//Modifiers
+	void clear(); //Removes all elements from the vector, leaving the container with a size of 0.
+	void insert(int ind, const T& val); // inserts the element(val) at given ind, increasing size by 1
+	
+	template <typename... Args>             //Inserts an element constructed in place into the vector at a specified position(ind).
+    	void emplace(int ind, Args&&... args);  //The arguments args... are forwarded to the constructor as std::forward<Args>(args)...
+
+	void erase(int ind); // erase element at given position(ind), decreasing size by 1 	
+	void push_back(const T& val); // Adds a new element(num) at the end of the vector,
 
 	template <typename... Args>         //Adds an element constructed in place to the end of the vector
     	void emplace_back(Args&&... args);  //The arguments args... are forwarded to the constructor as std::forward<Args>(args)...
-     					   
+
+	void pop_back(); // Removes the last element in the vector, redusing m_size by 1,
 
 
-	template <typename... Args>             //Inserts an element constructed in place into the vector at a specified position(ind).
-    	void emplace(int ind, Args&&... args);  //The arguments args... are forwarded to the constructor as std::forward<Args>(args)...
-	
+	void swap (Vec& other); /*Exchanges the content of the container by the content of other, which is another Vec object of the same 
+				type. Sizes may differ.*/		
 private:	
 	void realloc(size_t new_cap = 0); // reallocates memory, if no argument passed doubles the cap, else cap is equal to new_cap
 
